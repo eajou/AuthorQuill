89365287e1bb1b60cbf5a098de2d8ed30f5673d4
Krystal <krystal@Krystals-Air.hitronhub.home>
2020-09-19 22:19:03 -0400

    series page setup

--- wp-content/themes/authorQuill/footer.php
+++ wp-content/themes/authorQuill/footer.php
@@ -1,19 +1,33 @@
-			<!-- footer -->
-			<footer class="footer" role="contentinfo">
+<footer>
+	<section class="damask">
+		<div class="space"></div>
+  </section>
+  
+  <div class="container">
+    <div class="social">
+      <div class="icon">
+        <a href="https://www.facebook.com/lylaoweds/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+      </div>
+      
+      <div class="icon">
+        <a href="https://www.instagram.com/lyla.oweds/" target="_blank"><i class="fab fa-instagram"></i></a>
+      </div>
+      
+      <div class="icon">
+        <a href="https://www.amazon.com/author/lylaoweds" target="_blank"><i class="fab fa-amazon"></i></a>
+      </div>
+      
+      <div class="icon">
+        <a href="https://www.goodreads.com/eajou" target="_blank"><i class="fab fa-goodreads-g"></i></a>
+      </div>
+    </div>
+    
+    <p>Copyright © 2018-2020 Lyla Oweds. All rights reserved.<br />
+    Website design and development done by Lyla Oweds.<br />
+    <a href="/contact">Contact me</a> for quotes.</p>
+  </div>
 
-				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'lyla'); ?>
-					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//lyla.com" title="lyla">Lyla</a>.
-				</p>
-				<!-- /copyright -->
-
-			</footer>
-			<!-- /footer -->
-
-		</div>
-		<!-- /wrapper -->
-
-		<?php wp_footer(); ?>
-	</body>
+  <?php wp_footer(); ?>
+  </body>
+</footer>
 </html>
